44477dc6b382d27e21fa1be84e573cf457f1eff5
Theodore Smith <tjs7321@gmail.com>
2023-09-22 00:36:18 -0400

    links to detail page from homescreen now work

--- client/src/components/PrepSessionsTile.js
+++ client/src/components/PrepSessionsTile.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function PrepSessionsTile({title, start, end}) {
+function PrepSessionsTile({id, title, start}) {
+
+    function handleClick(){
+        console.log(`${id} was clicked!`)
+    }
+
     return(
         <div>
-            <h3>Session</h3>
-            <p>Title: {title}</p>
-            <p>Start Time: {start}</p>
-            <p>End Time: {end}</p>
+            <NavLink
+            to={`/sessions/${id}`}
+            >{title}</NavLink>
+            <p>{Date(start.slice(0, -1))}</p>
         </div>
         
     )
